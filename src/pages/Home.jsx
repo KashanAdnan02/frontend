2451488db5e303ca7ca948c4da9d2181070a8102
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-08-03 17:06:59 +0500

    done done dana done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,23 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
     const token = localStorage.getItem("token")
     const [data, setData] = useState({})
     console.log(token);
+    const navigate = useNavigate()
+
     async function getData() {
         const data = await axios.post("https://backend-smit-authentication.vercel.app/user", { token })
         console.log(data)
         setData(data.data)
     }
     useEffect(() => {
-        setTimeout(() => {
-            getData()
-        }, 4000);
+        if (!token) {
+            navigate("/signin")
+        }
+        getData()
     }, [])
 
     return (
